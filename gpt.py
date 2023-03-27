913a3bbf47d4e81449ad6334f4eb0d8ba6ee9eb3
Kelvin <kelvinkonoor@gmail.com>
2023-03-27 11:45:36 +0530

    function generate batches of len batch_size

--- gpt.py
+++ gpt.py
@@ -19,4 +19,17 @@ decode(encode('kelvin'))
 
 Xtrain_enc, Xtest_enc = torch.tensor(encode(Xtrain)), torch.tensor(encode(Xtest))
 
+def get_random_batch(choice = 'train', batch_size = 1):
+    if choice == 'train':
+        idxs = random.sample(range(0, len(Xtrain)), batch_size)
+        Xbatch = torch.stack([Xtrain_enc[idx:idx + block_size] for idx in idxs])
+        Ybatch = torch.stack([Xtrain_enc[idx + 1:idx + block_size + 1] for idx in idxs])
+        return Xbatch, Ybatch
+    elif choice == 'test':
+        idxs = random.sample(range(0, len(Xtest)), batch_size)
+        Xbatch = torch.stack([Xtest_enc[idx:idx + block_size] for idx in idxs])
+        Ybatch = torch.stack([Xtest_enc[idx + 1:idx + block_size + 1] for idx in idxs])
+        return Xbatch, Ybatch
 
+batch_size = 4
+Xbatch, Ybatch = get_random_batch('train', 4)
